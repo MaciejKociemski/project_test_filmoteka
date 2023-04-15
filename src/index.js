224c93ab24b2429bdc6f355ad17599e4f0dc4e10
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-04-15 08:42:48 +0200

    chore:added folders images and js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// styles
+import './scss/main.scss';
+import 'animate.css/animate.css';
+
+// scripts
+import './js/start-page';
+import './js/render/render-page-library'; 
+import './js/search';
+import './js/movie-on-modal';
+import './js/components/pagination';
+import './js/our-team-modal';
+import './js/components/back-to-top';
+
+// 
+import './js/components/theme-change';
+import './js/storage/local-storage_theme-switch';
